11f55ef546e6358fe297635160aa61aa1ed5eddc
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-04-09 15:40:27 +0700

    Add SplashScreen and Fixing Bug

--- app/src/main/java/com/zendev/gelamaitiara/data/FoodData.java
+++ app/src/main/java/com/zendev/gelamaitiara/data/FoodData.java
@@ -7,10 +7,10 @@ public class FoodData {
     public static String newline = System.getProperty("line.separator");
 
     public static String[][] data = new String[][]{
-            {"Beras Rendang", "Makanan Khas Payakumbuh", "Besar Rp. 7.000" + newline + "Menengah Rp. 5.000" + newline + "Kecil Rp. 3000", "Ini Baris Pertama" + newline + "baris Ke Dua", "https://2.bp.blogspot.com/-1UNwMYYLbWQ/XKoB9SRusqI/AAAAAAAAACo/vcNRJJPHQRALLSveZ8-h1gBTyhbqMwsKACLcBGAs/s1600/IMG_20190309_110315.jpg"},
-            {"Kipang", "Makanan Khas Payakumbuh", "Besar Rp. 7.000" + newline + "Menengah Rp. 5.000" + newline + "Kecil Rp. 3000", "Pendiri Microsoft ini Memiliki Total Kekayaan 90 Miliar Dollar AS", "https://1.bp.blogspot.com/-CYSBxPsIr68/XKoB7G5-leI/AAAAAAAAACg/mwtpag9T6mEBAmv9EZ4zqdIOA67ACoGuwCLcBGAs/s1600/IMG_20190309_110239.jpg"},
-            {"Batiah", "Makanan Khas Payakumbuh", "Besar Rp. 7.000" + newline + "Menengah Rp. 5.000" + newline + "Kecil Rp. 3000", "Pemilik Saham Berkshire Hathaway ini Memiliki Total Kekayaan 84 Miliar Dollar AS", "https://3.bp.blogspot.com/-dP-rsxz7D3c/XKoB89r93RI/AAAAAAAAACk/5RLFw_ntCOsBDxAiHNG4IrY8-ZrCYUARgCLcBGAs/s1600/IMG_20190309_110335.jpg"},
-            {"Gelamai", "Makanan Khas Payakumbuh", "Besar Rp. 7.000" + newline + "Menengah Rp. 5.000" + newline + "Kecil Rp. 3000", "Pemilik Brand Louis Vuitton ini Memiliki Total Kekayaan 72,2 Miliar Dollar AS", "https://2.bp.blogspot.com/-NDmQtpsBL6w/XKoLcb-DU6I/AAAAAAAAAC8/YnwLijoVne4axTJ3DEktZtutzWT1_r4OwCLcBGAs/s1600/InShot_20190407_213354348.jpg"}
+            {"Beras Rendang", "Makanan Khas Payakumbuh", "Besar Rp. 7.000" + newline + "Menengah Rp. 5.000" + newline + "Kecil Rp. 3000", "Beras Rendang Merupakan ....", "https://2.bp.blogspot.com/-1UNwMYYLbWQ/XKoB9SRusqI/AAAAAAAAACo/vcNRJJPHQRALLSveZ8-h1gBTyhbqMwsKACLcBGAs/s1600/IMG_20190309_110315.jpg"},
+            {"Kipang", "Makanan Khas Payakumbuh", "Besar Rp. 7.000" + newline + "Menengah Rp. 5.000" + newline + "Kecil Rp. 3000", "Kipang Merupakan ....", "https://1.bp.blogspot.com/-CYSBxPsIr68/XKoB7G5-leI/AAAAAAAAACg/mwtpag9T6mEBAmv9EZ4zqdIOA67ACoGuwCLcBGAs/s1600/IMG_20190309_110239.jpg"},
+            {"Batiah", "Makanan Khas Payakumbuh", "Besar Rp. 7.000" + newline + "Menengah Rp. 5.000" + newline + "Kecil Rp. 3000", "Batiah Merupakan ....", "https://3.bp.blogspot.com/-dP-rsxz7D3c/XKoB89r93RI/AAAAAAAAACk/5RLFw_ntCOsBDxAiHNG4IrY8-ZrCYUARgCLcBGAs/s1600/IMG_20190309_110335.jpg"},
+            {"Gelamai", "Makanan Khas Payakumbuh", "Besar Rp. 7.000" + newline + "Menengah Rp. 5.000" + newline + "Kecil Rp. 3000", "Gelamai Merupakan ....", "https://2.bp.blogspot.com/-NDmQtpsBL6w/XKoLcb-DU6I/AAAAAAAAAC8/YnwLijoVne4axTJ3DEktZtutzWT1_r4OwCLcBGAs/s1600/InShot_20190407_213354348.jpg"}
     };
 
     public static ArrayList<Food> getListData(){
